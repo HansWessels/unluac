eb77c54bd2dcfa03bf3074fbb76788c31dea94ed
Hans Wessels (Mr Ni!) <hans@suiker.org>
2015-07-11 16:57:34 +0200

    add bash build shell script to quickly compile and deploy unluac; also added Manifest file to aid the build process

---
+++ build.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+pushd "$( dirname "$0" )"			2> /dev/null > /dev/null
+
+#
+# compile all java sources:
+#
+mkdir build 						2> /dev/null > /dev/null
+#javac -verbose -deprecation -Werror -d build -sourcepath src  src/unluac/*.java
+javac -deprecation -Werror -d build -sourcepath src  src/unluac/*.java
+
+#
+# bundle in a JAR file with a suitable Manifest file:
+#
+# (Note #1: Note the -C parameter: without that you're fucked!)
+# (Note #2: also don't write the JAR file to the build directory or you'll have trouble re-building the next time -- or you have to spec your class files more precisely than this '.' at the end of the jar command below!)
+#
+mkdir bin 							2> /dev/null > /dev/null
+#jar -cvfm bin/unluac.jar  src/unluac/Manifest.mf  -C build  .
+jar -cfm bin/unluac.jar  src/unluac/Manifest.mf  -C build  .
+
+#
+# DEPLOY:
+#
+# and place the new compiled JAR file in the active tool directory:
+#
+cp bin/unluac.jar ../../whereigo_bulk_decompilercrypter/tools/
+
+popd								2> /dev/null > /dev/null
